daf96a213d7b88a0f9d190fff517536f666dd5b2
Ricardo Ramirez <rambellricardo@gmail.com>
2024-12-06 23:34:38 -0600

    Agregación de atributo colo en status

--- src/models/Status.ts
+++ src/models/Status.ts
@@ -3,11 +3,13 @@ import { Schema } from 'mongoose';
 export interface IStatus {
     name: string;
     description: string;
+    color: string;
     isActive: boolean;
 }
 
 export const StatusSchema: Schema = new Schema<IStatus>({
     name: { type: String, required: true },
     description: { type: String, required: true },
+    color: { type: String, required: true },
     isActive: { type: Boolean, required: true, default: true },
 });
